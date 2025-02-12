dc47d3b2c605b5b153ceea29d1a3190c958126b5
itsbasilsaman <itsbasilsaman@gmail.com>
2025-02-12 15:59:17 +0530

    changes made

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-// src/App.tsx
 import React, { Fragment, Suspense } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { Loading } from "./components/pages/Loading";
 import { AdminLogin } from "./components/forms/admin/login";
@@ -24,13 +23,19 @@ import GetUserById from "./components/pages/user-management/getUserById";
 import AddRegion from "./components/pages/Form/AddRegion";
 import RegionListSection from "./components/Table/RegionListSection";
 import GetOffer from "./components/pages/offer-management/getOffer";
- 
 
 export const App: React.FC = React.memo(() => {
   const { isLogged } = useSelector((state: RootState) => state.auth);
-  console.log('check is the user login ****** ', isLogged);
+  console.log('Check if the user is logged in: ', isLogged);
+
   const location = useLocation();
   const isLoginRoute = location.pathname === "/admin/login";
+
+  // Redirect to login page if user is not logged in
+  if (!isLogged && !isLoginRoute) {
+    return <Navigate to="/admin/login" replace />;
+  }
+
   return (
     <Fragment>
       <Toaster position="top-center" />
@@ -39,125 +44,27 @@ export const App: React.FC = React.memo(() => {
           <Route path="/admin/login" element={<AdminLogin />} />
         </Routes>
 
-        {!isLoginRoute && (
+        {!isLoginRoute && isLogged && (
           <DefaultLayout>
             <Routes>
               <Route index element={<ECommerce />} />
-              <Route
-                path="/seller/sellerList"
-                element={
-                  <>
-                    <SellerList />
-                  </>
-                }
-              />
-              <Route
-                path="/brand"
-                element={
-                  <>
-                    <BrandData />
-                  </>
-                }
-              />
-              <Route
-                path="/addservice"
-                element={
-                  <>
-                    <AddService />
-                  </>
-                }
-              />
-              <Route
-                path="/brandAdd"
-                element={
-                  <>
-                    <BrandData />
-                  </>
-                }
-              />
-              <Route
-                path="/sellerlist"
-                element={
-                  <>
-                    <SellerListSection />
-                  </>
-                }
-              />
-              <Route
-                path="/sellerprofile"
-                element={
-                  <>
-                    <SellerProfile />
-                  </>
-                }
-              />
-              <Route
-                path="/subserviceform"
-                element={
-                  <>
-                    <SubServiceForm />
-                  </>
-                }
-              />
-              <Route
-                path="/brandlist"
-                element={
-                  <>
-                    <BrandList />
-                  </>
-                }
-              />
-              <Route
-                path="/brand-detail"
-                element={
-                  <>
-                    <BrandDetail />
-                  </>
-                }
-              />
-              <Route
-                path="/servicelistsection"
-                element={
-                  <>
-                    <ServiceListSection />
-                  </>
-                }
-              />
-
-              <Route
-                path="/userlist"
-                element={
-                  <>
-                    <UserList/>
-                  </>
-                }
-              />
-
-              <Route
-                path="/getuser"
-                element={
-                  <>
-                  <GetUserById/>
-                  </>
-                }
-              />
-
-              <Route
-                path="/addProduct"
-                element={
-                  <>
-                    <AddProduct />
-                  </>
-                }
-              />
+              <Route path="/seller/sellerList" element={<SellerList />} />
+              <Route path="/brand" element={<BrandData />} />
+              <Route path="/addservice" element={<AddService />} />
+              <Route path="/brandAdd" element={<BrandData />} />
+              <Route path="/sellerlist" element={<SellerListSection />} />
+              <Route path="/sellerprofile" element={<SellerProfile />} />
+              <Route path="/subserviceform" element={<SubServiceForm />} />
+              <Route path="/brandlist" element={<BrandList />} />
+              <Route path="/brand-detail" element={<BrandDetail />} />
+              <Route path="/servicelistsection" element={<ServiceListSection />} />
+              <Route path="/userlist" element={<UserList />} />
+              <Route path="/getuser" element={<GetUserById />} />
+              <Route path="/addProduct" element={<AddProduct />} />
               <Route path="/subservice" element={<SubService />} />
-              <Route path="/subservice" element={<SubService />} />
-
-              <Route path="/addregion" element={<AddRegion/>} />
-              <Route path="/regionlist" element={<RegionListSection/>} />
-
-              <Route path="/getoffer" element={<GetOffer/>} />
-
+              <Route path="/addregion" element={<AddRegion />} />
+              <Route path="/regionlist" element={<RegionListSection />} />
+              <Route path="/getoffer" element={<GetOffer />} />
             </Routes>
           </DefaultLayout>
         )}
